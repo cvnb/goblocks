dc8b507d18561dc77317b492c542e7911c978e5a
Narendra Bharathi C V <narendra.bharathi@gmail.com>
2020-09-26 12:11:13 -0400

    Make temperature module to be generic and to use specific file

--- lib/modules/temperature.go
+++ lib/modules/temperature.go
@@ -2,10 +2,9 @@ package modules
 
 import (
 	"fmt"
-	"github.com/davidscholberg/go-i3barjson"
-	"io/ioutil"
 	"os"
-	"strings"
+
+	"github.com/davidscholberg/go-i3barjson"
 )
 
 // Temperature represents the configuration for the CPU temperature block.
@@ -13,7 +12,7 @@ import (
 // e.g. /sys/devices/platform/coretemp.0/hwmon
 type Temperature struct {
 	BlockConfigBase `yaml:",inline"`
-	CpuTempPath     string  `yaml:"cpu_temp_path"`
+	TempPath        string  `yaml:"cpu_temp_path"`
 	CritTemp        float64 `yaml:"crit_temp"`
 }
 
@@ -22,63 +21,35 @@ type Temperature struct {
 func (c Temperature) UpdateBlock(b *i3barjson.Block) {
 	b.Color = c.Color
 	fullTextFmt := fmt.Sprintf("%s%%s", c.Label)
-	totalTemp := 0
-	procs := 0
-	sysFileDirList, err := ioutil.ReadDir(c.CpuTempPath)
+	temp := 0
+
+	_, err := os.Stat(c.TempPath)
 	if err != nil {
 		b.Urgent = true
 		b.FullText = fmt.Sprintf(fullTextFmt, err.Error())
 		return
 	}
-	if len(sysFileDirList) != 1 {
-		b.Urgent = true
-		msg := fmt.Sprintf(
-			"in %s, expected 1 file, got %d",
-			c.CpuTempPath,
-			len(sysFileDirList),
-		)
-		b.FullText = fmt.Sprintf(fullTextFmt, msg)
-		return
-	}
-	sysFileDirPath := fmt.Sprintf(
-		"%s/%s",
-		c.CpuTempPath,
-		sysFileDirList[0].Name(),
-	)
-	sysFileNameFmt := fmt.Sprintf("%s/%%s", sysFileDirPath)
-	sysFiles, err := ioutil.ReadDir(sysFileDirPath)
+
+	r, err := os.Open(c.TempPath)
 	if err != nil {
 		b.Urgent = true
 		b.FullText = fmt.Sprintf(fullTextFmt, err.Error())
 		return
 	}
-	for _, sysFile := range sysFiles {
-		sysFileName := sysFile.Name()
-		if !strings.HasSuffix(sysFileName, "input") {
-			continue
-		}
-		r, err := os.Open(fmt.Sprintf(sysFileNameFmt, sysFileName))
-		if err != nil {
-			b.Urgent = true
-			b.FullText = fmt.Sprintf(fullTextFmt, err.Error())
-			return
-		}
-		defer r.Close()
-		var temp int
-		_, err = fmt.Fscanf(r, "%d", &temp)
-		if err != nil {
-			b.Urgent = true
-			b.FullText = fmt.Sprintf(fullTextFmt, err.Error())
-			return
-		}
-		totalTemp += temp
-		procs++
+	defer r.Close()
+
+	_, err = fmt.Fscanf(r, "%d", &temp)
+	if err != nil {
+		b.Urgent = true
+		b.FullText = fmt.Sprintf(fullTextFmt, err.Error())
+		return
 	}
-	avgTemp := float64(totalTemp) / float64(procs*1000)
-	if avgTemp >= c.CritTemp {
+
+	calcTemp := float64(temp) / float64(1000)
+	if calcTemp >= c.CritTemp {
 		b.Urgent = true
 	} else {
 		b.Urgent = false
 	}
-	b.FullText = fmt.Sprintf("%s%.2f°C", c.Label, avgTemp)
+	b.FullText = fmt.Sprintf("%s%.2f°C", c.Label, calcTemp)
 }
